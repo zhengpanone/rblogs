425c9db70471244217ceefa810c17b6058e3e01c
zhengpanone <zhengpanone@hotmail.com>
2020-10-02 22:33:12 +0800

    复合类型

--- source/contents/g01_语言基础/code/g02/02变量定义和使用.go
+++ source/contents/g01_语言基础/code/g02/02变量定义和使用.go
@@ -5,15 +5,16 @@ package main
 import "fmt"
 
 // 程序的主入口 程序有且只有一个主函数
-func main()  {
+func main() {
 	// var PI float64 = 3.14159
-	PI := 3.14159 //自动推导类型
+	var name, age = "张三", 18 //类型推导
+	PI := 3.14159            //简短变量声明,只能在函数中使用
 	var r float64 = 3
 	//var s float64 //声明
 	//计算面积
-	s := PI*r*r 
+	s := PI * r * r
 
-	fmt.Println("面积",s)
-	
+	fmt.Println("面积", s)
+	fmt.Printf("名字：%s \t 年龄:%d", name, age)
+
 }
-
--- source/contents/g01_语言基础/code/g02/第一个go程序.go
+++ source/contents/g01_语言基础/code/g02/第一个go程序.go
@@ -1,9 +1,8 @@
-// 导入主函数的包
+// 包, 表明代码所在的模块
 package main
 
 //系统导入所需要的包format 标注输入输出格式包
 import "fmt"
-
 
 // 行注释
 
@@ -11,14 +10,30 @@ import "fmt"
 块注释
 */
 
+// 函数外面只能放标识符(变量、常量、函数、类型)的声明,不能放语句
+
 var b int // 声明
 // 定义变量
 var a int = 10
 
+// 批量声明
+var (
+	name string
+	age  int
+	isOK bool
+)
+
 // 程序主入口,程序有且只有一个主函数
-func main()  {
+func main() {
+	// GO语言变量声明必须使用,非全局变量声明了必须使用
+	name = "张三"
+	age = 15
+	isOK = true
 	fmt.Print("Hello World!")
 	fmt.Println("======================")
 	fmt.Println(a)
 	fmt.Println(b)
+
+	fmt.Println(name)
+
 }
