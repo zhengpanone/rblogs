469aa6c2cdc94e05326af6f10cf9ab58cefb73ff
zhengpanone <zhengpanone@hotmail.com>
2026-05-24 00:30:30 +0800

    提交代码

--- source/exts/zh.py
+++ source/exts/zh.py
@@ -5,7 +5,8 @@ from smallseg import SEG
 class SearchChinese(SearchLanguage):
     lang = 'zh'
 
-    def init(self, options):
+    def __init__(self, options):
+        super().__init__(options)
         print("reading Chinese dictionary")
         self.seg = SEG() 
 
